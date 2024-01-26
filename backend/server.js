b2ed6474d10c4b0b322d885a4718d16f32bfb3c6
llja <lesterlim.ja@gmail.com>
2024-01-26 20:41:01 +0800

    feat:set up socket.io server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,34 @@
-const express = require("express");
+require('./environment');
+const express = require('express');
+const cors = require('cors');
+const { Server } = require("socket.io");
+const {createServer} = require('http');
 
-const PORT = 8080;
+
+
+const PORT = process.env.PORT || 8080;
 const app = express();
+const http = createServer(app);
+const io = new Server(http,{
+  cors:{
+    origin: '*',
+    methods: ['GET','POST'],
+  }
+})
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello, Shall WeTalk!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Express app listening on port ${PORT}!`);
+
+
+io.on('connection', socket => {
+  console.log("user id:",socket.id," has logged in!")
+})
+
+http.listen(PORT,'0.0.0.0',()=>{
+  console.log(`🚀 App listening on the port ${PORT}`);
 });
