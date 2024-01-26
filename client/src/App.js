c7890bbd10dc54f7974e2a9e2607a81ba9f30cb7
llja <lesterlim.ja@gmail.com>
2024-01-26 20:41:34 +0800

    feat:set up frontend socket.io response

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React , {useEffect} from "react";
+import io from 'socket.io-client';
 
 const App = () => {
+  useEffect(()=>{
+    const newSocket = io('http://localhost:8080')
+  })
   return (
     <div className="App mt-12 flex items-center justify-center text-3xl text-gray-500">
       <h1>Welcome to Shall Wetalk</h1>
