10389358ba9af6a11c6224315f16b349cb67263a
Cathy Hui <erashine414@icloud.com>
2024-02-06 15:22:23 +0800

    updated sidebar for chat page

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -13,27 +13,27 @@ const Sidebar = () => {
   return (
     <aside className={`h-screen ${isCollapsed ? "w-16" : "w-1/6"}`}>
       <nav
-        className={`h-full flex flex-col ${
+        className={`h-full  flex flex-col ${
           isCollapsed ? "bg-sky-600" : "bg-sky-500"
         } border-r border-gray-100 shadow-sm`}
       >
-        <div className="p-4 flex justify-between items-center">
+        <div className="p-4 mt-4 flex justify-between items-center">
           {!isCollapsed && <FaRegPaperPlane className="text-2xl text-white" />}
           {!isCollapsed && (
             <p className="text-md text-white font-bold">Shall WeTalk</p>
           )}
           <button onClick={toggleSidebar}>
             {isCollapsed ? (
-              <PiCaretCircleRightBold className="text-2xl text-white hover:text-gray-600" />
+              <PiCaretCircleRightBold className="text-2xl text-white " />
             ) : (
-              <PiCaretCircleLeftBold className="text-2xl text-white hover:text-gray-600" />
+              <PiCaretCircleLeftBold className="text-2xl text-white " />
             )}
           </button>
         </div>
         <ul className={`flex-1 ${isCollapsed ? "px-1" : "px-2"}`}>
           <SidebarItems isCollapsed={isCollapsed} />
         </ul>
-        <div className="border-t p-4 flex items-center">
+        <div className=" p-4 flex items-center">
           <img
             src="https://images.unsplash.com/photo-1600807497639-3b5d8e74a232?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGhpa2luZ3xlbnwwfHwwfHx8MA%3D%3D"
             alt="dummy user image"
--- client/src/components/SidebarItems.jsx
+++ client/src/components/SidebarItems.jsx
@@ -14,29 +14,29 @@ const SidebarItems = ({ isCollapsed }) => {
   return (
     <div className="text-white mt-4">
       {/* You can add or remove items as needed */}
-      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold">
+      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold cursor-pointer duration-100 cursor-pointer duration-100">
         <PiHouseBold className="text-xl" />
-        {!isCollapsed && <span className="ml-4">Home</span>}
+        {!isCollapsed && <span className="ml-4 ">Home</span>}
       </li>
-      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold">
+      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold cursor-pointer duration-100">
         <PiChatBold className="text-xl" />
-        {!isCollapsed && <span className="ml-4">Messages</span>}
+        {!isCollapsed && <span className="ml-4 ">Messages</span>}
       </li>
-      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold">
+      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold cursor-pointer duration-100">
         <PiVideoBold className="text-xl" />
-        {!isCollapsed && <span className="ml-4">Video Chat</span>}
+        {!isCollapsed && <span className="ml-4 ">Video Chat</span>}
       </li>
-      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold">
+      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold cursor-pointer duration-100">
         <PiUserBold className="text-xl" />
-        {!isCollapsed && <span className="ml-4">Profile</span>}
+        {!isCollapsed && <span className="ml-4 ">Profile</span>}
       </li>
-      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold">
+      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold cursor-pointer duration-100">
         <PiBellRingingBold className="text-xl" />
-        {!isCollapsed && <span className="ml-4">Notifications</span>}
+        {!isCollapsed && <span className="ml-4 ">Notifications</span>}
       </li>
-      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold">
+      <li className="flex items-center p-4 hover:bg-sky-400 rounded-md font-semibold cursor-pointer duration-100">
         <PiGearBold className="text-xl" />
-        {!isCollapsed && <span className="ml-4">Settings</span>}
+        {!isCollapsed && <span className="ml-4 ">Settings</span>}
       </li>
     </div>
   );
