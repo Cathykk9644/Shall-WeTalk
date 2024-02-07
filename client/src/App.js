b34b8376e50d65012c3db65f49e25feb45fac4b0
llja <lesterlim.ja@gmail.com>
2024-02-08 02:37:20 +0800

    feat:framing userProfile page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
+import Chat from "./pages/Chat";
 
 const App = () => {
   useEffect(() => {
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/chat" element={<Chat />} />
           <Route path="/userprofile" element={<UserProfile />} />
         </Routes>
       </div>
