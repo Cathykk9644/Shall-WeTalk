19b262f00728870377339354f32da975d3b2273b
zachariahkozlowski-best <mcfeegles@gmail.com>
2024-02-08 21:34:00 +0800

    feat: added frontend validation to signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,16 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import loginbg from "../Assets/loginbg.avif";
-import logo from "../Assets/Logo.jpeg";
-import { motion } from "framer-motion";
-import loginImg from "../Assets/landingbg3.png";
-import { FaGooglePlus } from "react-icons/fa6";
-import { FaFacebook } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { RiChatSmile2Line } from "react-icons/ri";
+import React from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import loginbg from '../Assets/loginbg.avif';
+import logo from '../Assets/Logo.jpeg';
+import { motion } from 'framer-motion';
+import loginImg from '../Assets/landingbg3.png';
+import { FaGooglePlus } from 'react-icons/fa6';
+import { FaFacebook } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { RiChatSmile2Line } from 'react-icons/ri';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { passwordSchema } from '../utils/validationFuncts.js';
+
+const schema = z
+  .object({
+    username: z.string().min(4, {
+      message: 'Your username must be at least 4 characters long',
+    }),
+    email: z.string().email('Please enter a valid email'),
+    password: passwordSchema,
+    passwordConfirmation: z.string(),
+    isVerrified: z.boolean().optional(),
+  })
+  .superRefine(({ passwordConfirmation, password }, ctx) => {
+    if (passwordConfirmation !== password) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'The passwords did not match',
+        path: ['passwordConfirmation'],
+      });
+    }
+  });
 
 const Signup = () => {
   const navigate = useNavigate();
+
+  const onSubmit = (data) => {
+    console.log('onSubmit:', data);
+    alert('I am being called');
+  };
+
+  const { handleSubmit, register, formState } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const { errors } = formState;
 
   return (
     <div className="flex min-h-screen">
@@ -22,7 +57,7 @@ const Signup = () => {
         {/* Transparent container */}
         <div
           className="bg-white  bg-opacity-20 rounded-lg p-8 backdrop-filter backdrop-blur-md"
-          style={{ backdropFilter: "blur(5px)" }}
+          style={{ backdropFilter: 'blur(5px)' }}
         >
           <div className="flex space-x-4 ">
             <RiChatSmile2Line className="text-white text-5xl" />
@@ -38,12 +73,12 @@ const Signup = () => {
           <p className="text-3xl font-semibold text-white mb-16">
             Right Here On Shall Wetalk!
           </p>
-        </div>{" "}
+        </div>{' '}
         <img
           src={loginImg}
           alt="loginImg"
           className="absolute bottom-20 left-12 m-4 max-w-xs object-cover opacity-40"
-          style={{ maxWidth: "220px" }}
+          style={{ maxWidth: '220px' }}
         />
       </div>
 
@@ -54,66 +89,76 @@ const Signup = () => {
         <h1 className="text-xl font-bold text-sky-500 items-center">
           Let's start to create account
         </h1>
-        <form className="flex flex-col space-y-4">
+        <form
+          className="flex flex-col space-y-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="w-full">
             <label className="block text-gray-400 text-xs font-semibold">
               Username
+              <input
+                className="h-10 block w-full mt-2 rounded-md border-0 p-4 text-gray-500 text-xs shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-0 focus:ring-1 focus:ring-inset focus:ring-sky-600"
+                name="username"
+                type="text"
+                placeholder="Username"
+                {...register('username')}
+                required
+              />
+              {errors.username && (
+                <p style={{ color: 'red' }}>{errors.username?.message}</p>
+              )}
             </label>
-            <input
-              className="h-10 block w-full mt-2 rounded-md border-0 p-4 text-gray-500 text-xs shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-0 focus:ring-1 focus:ring-inset focus:ring-sky-600"
-              name="password"
-              type="password"
-              placeholder="Username"
-              // value={password}
-              // onChange={(e) => onChange(e)}
-              required
-            />
           </div>
           <div className="w-full">
             <label className="block text-gray-400 text-xs font-semibold">
               Email
+              <input
+                className="h-10 block w-full mt-2 rounded-md border-0 p-4 text-gray-500 text-xs shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-0 focus:ring-1 focus:ring-inset focus:ring-sky-600"
+                name="email"
+                type="email"
+                placeholder="Email"
+                {...register('email')}
+                required
+              />
+              {errors.email && (
+                <p style={{ color: 'red' }}>{errors.email?.message}</p>
+              )}
             </label>
-            <input
-              className="h-10 block w-full mt-2 rounded-md border-0 p-4 text-gray-500 text-xs shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-0 focus:ring-1 focus:ring-inset focus:ring-sky-600"
-              name="email"
-              type="email"
-              placeholder="Email"
-              // value={email}
-              // onChange={(e) => onChange(e)}
-              required
-            />
           </div>
-
           <div className="w-full">
             <label className="block text-gray-400 text-xs font-semibold">
               Password
+              <input
+                className="h-10 block w-full mt-2 rounded-md border-0 p-4 text-gray-500 text-xs shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-0 focus:ring-1 focus:ring-inset focus:ring-sky-600"
+                name="password"
+                type="password"
+                {...register('password')}
+                placeholder="Password"
+                required
+              />
+              {errors.password && (
+                <p style={{ color: 'red' }}>{errors.password?.message}</p>
+              )}
             </label>
-            <input
-              className="h-10 block w-full mt-2 rounded-md border-0 p-4 text-gray-500 text-xs shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-0 focus:ring-1 focus:ring-inset focus:ring-sky-600"
-              name="password"
-              type="password"
-              placeholder="Password"
-              // value={password}
-              // onChange={(e) => onChange(e)}
-              required
-            />
           </div>
-
           <div className="w-full">
             <label className="block text-gray-400 text-xs font-semibold">
               Confirm Password
+              <input
+                className="h-10 block w-full mt-2 rounded-md border-0 p-4 text-gray-500 text-xs shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-0 focus:ring-1 focus:ring-inset focus:ring-sky-600"
+                name="passwordConfirmation"
+                type="password"
+                {...register('passwordConfirmation')}
+                placeholder="Password Again"
+                required
+              />
             </label>
-            <input
-              className="h-10 block w-full mt-2 rounded-md border-0 p-4 text-gray-500 text-xs shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-0 focus:ring-1 focus:ring-inset focus:ring-sky-600"
-              name="password"
-              type="password"
-              placeholder="Password Again"
-              // value={password}
-              // onChange={(e) => onChange(e)}
-              required
-            />
+            {errors.passwordConfirmation && (
+              <p style={{ color: 'red' }}>
+                {errors.passwordConfirmation.message}
+              </p>
+            )}
           </div>
-
           <button
             type="submit"
             className="h-10 px-4 py-2 bg-sky-500 text-white rounded-md hover:bg-bgColor3 text-sm focus:outline-none font-semibold hover:scale-95"
@@ -131,9 +176,9 @@ const Signup = () => {
         </div>
         {/* Icons container */}
         <div className="flex justify-center space-x-8">
-          {" "}
+          {' '}
           <button className="text-3xl text-sky-400 hover:text-amber-400 hover:scale-90">
-            {" "}
+            {' '}
             <FaGooglePlus />
           </button>
           <button className="text-3xl text-sky-400 hover:text-sky-700 hover:scale-90">
@@ -145,10 +190,11 @@ const Signup = () => {
         </div>
         <div className="text-center">
           <p className="text-xs text-gray-400 mb-4">
-            Already have an account with us?{" "}
+            Already have an account with us?{' '}
             <span
               className="underline cursor-pointer"
-              onClick={() => navigate("/login")}
+              onClick={() => navigate('/login')}
+              role="link"
             >
               Log in now!
             </span>
