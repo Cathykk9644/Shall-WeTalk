998007a11f6a0a27c93c1708150822c2db464727
llja <lesterlim.ja@gmail.com>
2024-02-10 01:39:16 +0800

    feat:firebase storage is working

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -2,25 +2,33 @@ import React, { useContext, useEffect, useState } from 'react';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { storage } from '../firebase';
 
+const STORAGE_KEY = "profilePictures/"
 
 const UserProfile=()=>{
   const [profileURL,setProfileURL] = useState();
   const [fileInputFile,setFileInputFile]=useState();
 
   const getProfilePictureURL = async (userId) => {
-    const storageRef = ref(storage, `profilePictures/${userId}`);
+    const storageRef = ref(storage,`${STORAGE_KEY,userId}`);
     const downloadURL = await getDownloadURL(storageRef);
+    setProfileURL(downloadURL);
     return downloadURL;
   }
 
   const uploadProfilePicture = async (userId, file) => {
-    const storageRef = ref(storage, `profilePictures/${userId}`);
+    const storageRef = ref(storage,`${STORAGE_KEY,userId}`);
     await uploadBytes(storageRef, file);
   };
   
   const handleFileInput = (e) =>{
     console.log(e)
     setFileInputFile(e.target.files[0])
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    uploadProfilePicture("test",fileInputFile);
+    getProfilePictureURL("test");
   }
 
   return(
@@ -36,14 +44,17 @@ const UserProfile=()=>{
         <div>{"Insert Learning Languages here"}</div>
         <br/>
         <label>Profile Picture  :</label>
-        <div>{profileURL}</div>
+        <img className='rounded-full' src={profileURL} alt='profile pic' />
         <input 
           type='file'
           name='file'
           onChange={(e)=>handleFileInput(e)}
         />
         <br/>
-        <button className='bg-buttonColor rounded-md border-4 border-buttonColor'>
+        <button 
+          className='bg-buttonColor rounded-md border-4 border-buttonColor text-white'
+          onClick={handleSubmit}  
+        >
           Upload your profile picture here!
         </button>
       </form>
